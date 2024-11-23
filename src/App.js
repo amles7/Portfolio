b1bc08236018ef03a9a063e12f7daf4cfc691b69
Dahi Selma <Dahiselma@outlook.com>
2024-11-23 13:46:04 +0100

    modification de app.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Skills from './components/Skills';
-import Project from"./components/Projects";
+import Project from './components/Projects';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Portfolio">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/skills" element={<Skills />} />
-        <Route path="/Project" element={<Project />} />
-
+        <Route path="/project" element={<Project />} />
       </Routes>
     </Router>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
     <footer className="footer">
       <p>&copy; 2024 Selma Dahi. Tous droits réservés.</p>
       <div className="socials">
-      <a href="mailto:Dahiselma@outlook.fr" className="icon">Contactez-moi</a>
+      <a href="#" className="icon">Contactez-moi</a>
         <a href="https://github.com/amles7" className="icon">GitHub</a>
         <a href="#" className="icon">Twitter</a>
       </div>
